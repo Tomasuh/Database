b9b8ee93f174ae29f04f0359be1df147c50037bb
Tomasuh <axel.em.olsson@gmail.com>
2016-02-27 12:29:18 +0100

    Cleaned up example code some

--- examples/example.c
+++ examples/example.c
@@ -2,48 +2,35 @@
 
 int main()
 {
-    //Database myOwn;
     printf("Hello world!\n");
 
     //gcc example.c -o example -std=gnu99 -ldatabase
 
-    /*
-    Create following database structure:
-    Database name: My test database
-    Tables {column:type}:
-    Apa {kiosk:String}
-
-    Fisk {mes:int}
-    Korv {brod:float}
-
-
-    */
-    Name columns[3] = {"FIIIISK","OOOOOXE","KOOOORV"};
-    Element elems[3] = {"FIIIISK","OOOOOXE","KOOOORV"};
+    Name columns[3] = {"column1","column2","column3"};
+    Element elems[3] = {"elem1","elem2","elem3"};
     Type typess[3] = {DB_STRING,DB_STRING,DB_STRING};
+
     Database *db;
-    //db = malloc(sizeof(Database));
+
     db_Create(&db, "Databasen");
     printf("%s",db->name);
 
     int ec;
 
-    Name tbNames[] = {"Apa","Fisk","Ko"};
+    Name tbNames[] = {"table1","table2","table3"};
     ec = db_AddTables(db, tbNames, 3);
     //printf("%s",db->tables[0]->name);
 
-    //db_AddColumn(db,"Apa","fjong", 1);
-
-    int ret = db_AddColumns(db,"Apa",columns,3,typess);
+    int ret = db_AddColumns(db,"table1",columns,3,typess);
     
     for(int i=0; i < 2000000; i++){
-        ret = db_insert(db, "Apa", columns, 3, elems);
+        ret = db_insert(db, "table1", columns, 3, elems);
     }
 
     db_free_database(&db);
 
     return 0;
-    printf("apa%d",ret);
+    printf("ret%d",ret);
     //int db_AddColumns(Database *db, Name table, Name *columns, int nrOfColumns, Type *columnType){
     /*printf("Column type:%d\n", db->tables[0]->columns[2]->type);
     printf("Column type:%d\n", db->tables[0]->columns[1]->type);
@@ -56,7 +43,6 @@ int main()
     //(Database *db, Name table, Name column, Element element)
 
     printf("%d ret", ret);
-    //ret = db_insert(db, "Apa", columns, 3, elems);
     printf("%d ret", ret);
     //db_insert()
     //printf("nrElem%d",db->tables[0]->columns[0]->nrOfElements);
